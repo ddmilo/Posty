7fb9d1758c266362dedee25b7f5442740a8810ff
Dan Milo <danmilo.dev@gmail.com>
2022-11-29 23:34:08 -0500

    Added more styling to ToDoList

--- client/src/components/TodosList.js
+++ client/src/components/TodosList.js
@@ -1,17 +1,21 @@
-import { Card, CardContent, Typography } from '@mui/material'
+import { Card, CardContent, Checkbox, FormControlLabel, FormGroup, IconButton, Typography } from '@mui/material'
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
 import React, {useState, useEffect} from 'react'
+import './../styles/TodosList.css'
+import ButtonGroup from '@mui/material/ButtonGroup';
 
 function TodosList(props) {
   return (  
     <div>
       TO DO LIST
       {props.todos.length > 0 ? 
-      <div>
+      <div className='cardContainer'>
 
         {props.todos.map((todo) => (
           
             
-          <Card key={todo.id} sx={{minWidth: 275}}>
+          <Card className='.card' key={todo.id} hover sx={{minWidth: 275}}>
             <CardContent>
               <Typography sx={{fontSize: 14}}>Task</Typography>
               <Typography variant="h5" component="div">
@@ -20,6 +24,17 @@ function TodosList(props) {
               <Typography variant="body2">
                   {todo.description}
               </Typography>
+              <FormGroup>
+                <FormControlLabel control= {<Checkbox />} label='Completed?' />
+              </FormGroup>
+              <ButtonGroup className='cardButtons'>
+                <IconButton>
+                  <EditIcon />
+                </IconButton>
+                <IconButton>
+                  <DeleteIcon />
+                </IconButton>
+              </ButtonGroup>
             </CardContent>
             {/* <h3>Created: {todo.created_at.toLocaleString('en')}</h3> */}
           </Card>
